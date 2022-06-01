4b64cd99d2bf95723682629e1fd49d5c47a7aaca
Delta <cebracho94@gmail.com>
2022-06-01 00:02:02 -0400

    adding hook and neflix img

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -8,16 +8,11 @@ import axios from "axios"
 import { API_KEY } from "../../secrets"
 import { baseURL } from "../../hooks/base_URL"
 
-
 export default function MovieList({id, name}) {
   const [movies, setMovies] = React.useState([])
 
   React.useEffect(async ()=> {
-    const movieArr = await axios.get(`${baseURL}discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}&with_watch_monetization_types=flatrate`)
-    .then(res => {
-      console.log(res.data.results)
-      return res.data.results
-    })
+    const movieArr = await getMoviesByGenre(id)
     setMovies(movieArr)
   },[]) 
 
--- src/hooks/GetMoviesByGenre.js
+++ src/hooks/GetMoviesByGenre.js
@@ -3,7 +3,9 @@ import { API_KEY } from "../secrets"
 import { baseURL } from "./base_URL"
 
 export default async function getMoviesByGenre(genre){
-        const { data } = await axios(`${baseURL}discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genre}&with_watch_monetization_types=flatrate`);
-        const movies = data.results;
+        const movies = await axios.get(`${baseURL}discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genre}&with_watch_monetization_types=flatrate`)
+        .then(res => {
+          return res.data.results
+        })
         return movies
 }
